95ecdb995f6e74d64d7682e583dcd55cd7b5cbfc
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-12-11 07:25:15 +0900

    add basic interfaces

--- lib/qiita_ex/api/base.ex
+++ lib/qiita_ex/api/base.ex
@@ -14,6 +14,26 @@ defmodule QiitaEx.API.Base do
     |> response
   end
 
+  def get(access_token, path, params \\ [], headers \\ %{}) do
+    request(access_token, :get, path, params, headers)
+  end
+
+  def post(access_token, path, params \\ [], headers \\ %{}) do
+    request(access_token, :post, path, params, headers)
+  end
+
+  def patch(access_token, path, params \\ [], headers \\ %{}) do
+    request(access_token, :patch, path, params, headers)
+  end
+
+  def put(access_token, path, params \\ [], headers \\ %{}) do
+    request(access_token, :put, path, params, headers)
+  end
+
+  def delete(access_token, path, params \\ [], headers \\ %{}) do
+    request(access_token, :delete, path, params, headers)
+  end
+
   defp process_url(url) do
     "https://qiita.com/api/v2/" <> url
   end
