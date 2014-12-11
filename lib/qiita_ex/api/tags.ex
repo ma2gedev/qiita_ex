e11d0e517a4a4a168e11b5477e5f1591cfe1554c
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-12-11 13:41:54 +0900

    add tags api

---
+++ lib/qiita_ex/api/tags.ex
@@ -0,0 +1,19 @@
+defmodule QiitaEx.API.Tags do
+  @moduledoc """
+  Provides tags API interfaces.
+  """
+
+  import QiitaEx.API.Base
+
+  def list_tags(access_token, params \\ [], headers \\ []) do
+    get(access_token, "tags", params, headers)
+  end
+
+  def get_tag(access_token, tag_id, params \\ [], headers \\ []) do
+    get(access_token, "tags/#{tag_id}", params, headers)
+  end
+
+  def list_user_following_tags(access_token, user_id, params \\ [], headers \\ []) do
+    get(access_token, "users/#{user_id}/following_tags", params, headers)
+  end
+end
